4b9ee756ccb581c672a96a2636f1b89adfb04fcd
EArellanoIA <najerae006@gmail.com>
2023-07-30 13:25:50 -0600

    Update

--- src/Helpers/DataLoader.py
+++ src/Helpers/DataLoader.py
@@ -1,6 +1,10 @@
-import pandas
+import pandas as pd
 
 
 class Dataloader:
-    def __init__(self,path):
-        self.path=path
+    def __init__(self, path):
+        self.path = path
+
+    def load_data(self):
+        data = pd.read_csv(self.path)
+        return
